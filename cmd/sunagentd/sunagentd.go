6d9d253058ad150be4a76911ec539c47de835099
expinc <expinc@outlook.com>
2021-06-16 17:20:12 +0800

    Load log configurations from file config.conf

--- cmd/sunagentd/sunagentd.go
+++ cmd/sunagentd/sunagentd.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"expinc/sunagent/log"
+	"os"
+
+	"github.com/go-ini/ini"
+)
+
+const procName = "sunagentd"
+
+func exit(status int, msg interface{}) {
+	log.Fatal(msg)
+	os.Exit(status)
+}
 
 func main() {
-	fmt.Println("SunAgent")
+	// load config
+	config, err := ini.Load("config.conf")
+	if nil != err {
+		exit(1, err)
+	}
+
+	// init log
+	log.SetLevel(config.Section("LOG").Key("level").String())
+	fileLimitMb, err := config.Section("LOG").Key("filelimitmb").Int()
+	if nil != err {
+		exit(1, err)
+	}
+	log.SetRotateFileOutput("logs/"+procName+".log", fileLimitMb)
+	log.Info(procName)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/go-ini/ini v1.62.0
 	github.com/sirupsen/logrus v1.8.1
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )

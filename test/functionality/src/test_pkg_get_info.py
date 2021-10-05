39984bdf9d4506ab87e2e78b5daf46a6061deccb
expinc <expinc@outlook.com>
2021-10-05 16:14:37 +0800

    Add test cases of getting package info
    * Add python requirement "distro" to determine Linux distribution

---
+++ test/functionality/src/test_pkg_get_info.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import common
+import distro
+import http.client
+import platform
+import pytest
+from assertpy import assert_that
+from http import HTTPStatus
+
+@pytest.mark.skipif("Linux" != platform.system(), reason="Package management only implemented for linux")
+class TestPackageGetInfo:
+    @classmethod
+    def setup_class(cls):
+        if "ubuntu" == distro.id() or "debian" == distro.id():
+            cls.must_have_pkg = "apt"
+        # there may be "opensuse-leap" as distro.id()
+        # so the distribution is determined by substring for opensuse
+        elif "centos" == distro.id() or "opensuse" in distro.id():
+            cls.must_have_pkg = "rpm"
+
+    def test_ordinary(self):
+        try:
+            conn = http.client.HTTPConnection(common.HOST, common.PORT)
+            url = "/api/v1/package/" + self.must_have_pkg
+            conn.request("GET", url)
+            response = conn.getresponse()
+
+            data = common.get_successful_response(response, HTTPStatus.OK)
+            assert_that(data["name"]).is_equal_to(self.must_have_pkg)
+            assert_that(data["version"]).is_not_empty()
+            assert_that(data["architecture"]).is_not_empty()
+            assert_that(data["summary"]).is_not_empty()
+        finally:
+            conn.close()
+
+    def test_not_installed(self):
+        try:
+            conn = http.client.HTTPConnection(common.HOST, common.PORT)
+            url = "/api/v1/package/" + "notexist"
+            conn.request("GET", url)
+            response = conn.getresponse()
+
+            common.assert_failed_response(response, HTTPStatus.NOT_FOUND)
+        finally:
+            conn.close()

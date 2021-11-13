19b3a919ef017ed1861105e404d145f57e58682b
expinc <expinc@outlook.com>
2021-11-13 20:42:53 +0800

    Add logs on command executions

--- command/exec.go
+++ command/exec.go
@@ -2,7 +2,10 @@ package command
 
 import (
 	"bytes"
+	"expinc/sunagent/log"
+	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -11,15 +14,34 @@ const (
 	NoTimeout      = (1<<63 - 1) * time.Nanosecond
 )
 
+func logOnExecStart(program string, args []string, timeout time.Duration) {
+	content := fmt.Sprintf("Executing commad: program=%s, args=%s, timeout=%v", program, strings.Join(args, ", "), timeout)
+	log.Debug(content)
+}
+
+func logOnExecFinish(program string, args []string, timeout time.Duration, err error, stdOut string, stdErr string) {
+	content := fmt.Sprintf("Executed commad: program=%s, args=%s, timeout=%v, err=%v, stdout=%s, stderr=%s",
+		program,
+		strings.Join(args, ", "),
+		timeout,
+		err,
+		stdOut,
+		stdErr,
+	)
+	log.Debug(content)
+}
+
 // Execute command "program" with arguments "args".
 // Stop the command process after "timeout" and return common.Error with code ErrorTimeout.
 // If command execution returns non-zero, this function will return a *os/exec.ExitError.
 func CheckCall(program string, args []string, timeout time.Duration) error {
+	logOnExecStart(program, args, timeout)
 	cmd := exec.Command(program, args...)
 	err := cmd.Start()
 	if nil == err {
 		err = waitForTimeout(cmd, timeout)
 	}
+	logOnExecFinish(program, args, timeout, err, "<ignored>", "<ignored>")
 	return err
 }
 
@@ -28,6 +50,7 @@ func CheckCall(program string, args []string, timeout time.Duration) error {
 // Stop the command process after "timeout" and return common.Error with code ErrorTimeout.
 // If command execution returns non-zero, this function will return a *os/exec.ExitError.
 func CheckCombinedOutput(program string, args []string, timeout time.Duration) (output []byte, err error) {
+	logOnExecStart(program, args, timeout)
 	cmd := exec.Command(program, args...)
 	var buffer bytes.Buffer
 	cmd.Stdout = &buffer
@@ -38,6 +61,7 @@ func CheckCombinedOutput(program string, args []string, timeout time.Duration) (
 		err = waitForTimeout(cmd, timeout)
 	}
 	output = buffer.Bytes()
+	logOnExecFinish(program, args, timeout, err, string(output), "<combined with output>")
 	return
 }
 
@@ -46,6 +70,7 @@ func CheckCombinedOutput(program string, args []string, timeout time.Duration) (
 // Stop the command process after "timeout" and return common.Error with code ErrorTimeout.
 // If command execution returns non-zero, this function will return a *os/exec.ExitError.
 func CheckSeparateOutput(program string, args []string, timeout time.Duration) (stdout []byte, stderr []byte, err error) {
+	logOnExecStart(program, args, timeout)
 	cmd := exec.Command(program, args...)
 	var bufferOut bytes.Buffer
 	var bufferErr bytes.Buffer
@@ -58,5 +83,6 @@ func CheckSeparateOutput(program string, args []string, timeout time.Duration) (
 	}
 	stdout = bufferOut.Bytes()
 	stderr = bufferErr.Bytes()
+	logOnExecFinish(program, args, timeout, err, string(stdout), string(stderr))
 	return
 }
